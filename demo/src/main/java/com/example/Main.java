0e2bf147e6b9eb3321eefda4bb1719d9eaf92725
rjviernes620 <rjviernes620@gmail.com>
2024-06-02 17:47:30 +0100

    Added Images, made popup for manual shift and added month list

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import javax.swing.text.DefaultCaret;
 import java.io.*;
 import java.util.*;
+import java.time.Year;
 
 public class Main extends JFrame {
 
@@ -16,13 +17,22 @@ public class Main extends JFrame {
 
     //GUI Elements
     private JTextArea confirmed_shift_profile = new JTextArea (20,17);
-
+    private ImageIcon uni_logo = new ImageIcon("demo\\src\\main\\resources\\uni_logo.png");
+    private JLabel uni_logo_label = new JLabel(uni_logo);  
     private JTextArea pre_added_shifts = new JTextArea (10, 40);
+    String[] monthList = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    
 
     //Buttons
     JButton generateTimesheet = new JButton ("Generate Timesheet");
     JButton budgetcode = new JButton ("Edit Budget Code");
     JButton manual_add = new JButton ("Manual Add");
+
+    //Manual Add -> Text Fields
+    private JTextArea shift_name = new JTextArea(1, 10);
+    private JTextArea budget_code = new JTextArea(1, 10);
+
+    JButton manual_add_shift = new JButton("Add Shift");
 
     final static boolean RIGHT_TO_LEFT = false;
 
@@ -78,6 +88,13 @@ public class Main extends JFrame {
         c.insets = new Insets(250, 0, 15, 225);
         mainbg.add(budgetcodeScroll, c);
 
+        //University Logo
+        c.fill = GridBagConstraints.FIRST_LINE_START;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(0, 0, 360, 140);
+        mainbg.add(uni_logo_label, c);
+
         //Confirmed Shifts
         confirmed_shift_profile.setEditable(false);
         confirmed_shift_profile.setLineWrap(true);
@@ -97,6 +114,20 @@ public class Main extends JFrame {
         c.gridx = 4;
         c.gridy = 0;
         mainbg.add(manual_addScroll, c);
+
+        //Month List
+        JComboBox<String> monthListScroll = new JComboBox<>(monthList);
+        monthListScroll.setMaximumRowCount(monthList.length);
+
+        Dimension preferredSize = monthListScroll.getPreferredSize();
+        preferredSize.width = 10; // Replace 100 with your desired width
+        monthListScroll.setMaximumSize(preferredSize);
+
+        c.fill = GridBagConstraints.FIRST_LINE_START;
+        c.gridx = 3;
+        c.gridy = 0;
+        c.insets = new Insets(0,0,0,0);
+        mainbg.add(monthListScroll, c);
 
         //Pre-Added Shifts
         pre_added_shifts.setEditable(false);
@@ -119,7 +150,53 @@ public class Main extends JFrame {
         mainbg.add(generateTimesheetScroll, c);
     }
 
+    
+
+    public void addComponentstoManualAdd() {
+            JFrame manualAddFrame = new JFrame("Manually add shift");
+            manualAddFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            manualAddFrame.setSize(500, 300);
+            manualAddFrame.setVisible(true);
+            manualAddFrame.setResizable(false);
+            manualAddFrame.setLocationRelativeTo(null);
+
+            manualAddFrame.setLayout(new GridBagLayout());
+            GridBagConstraints c = new GridBagConstraints();
+
+            shift_name.setEditable(true);
+            shift_name.setLineWrap(true);
+            shift_name.setWrapStyleWord(true);
+            shift_name.setText("Shift Name");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = 0;
+            manualAddFrame.add(shift_name, c);
+
+            budget_code.setEditable(true);
+            budget_code.setLineWrap(true);
+            budget_code.setWrapStyleWord(true);
+            budget_code.setText("Budget Code");
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.gridx = 0;
+            c.gridy = 1;
+            manualAddFrame.add(budget_code, c);
+    }
+
+    public Main() {
+
+        Container content = getContentPane();
+        getContentPane().setBackground(Color.decode("#f0f0f0"));
+
+        manual_add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                addComponentstoManualAdd();
+            }
+        });
+        }
+    
+
     public static void main(String[] args) {
+        ImageIcon small = new ImageIcon("demo\\src\\main\\resources\\small.png");
         Main frame = new Main();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.addComponentsToPane(frame.getContentPane());
@@ -129,5 +206,7 @@ public class Main extends JFrame {
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
+        Image sml = small.getImage();
+        frame.setIconImage(sml);
     }
 }
